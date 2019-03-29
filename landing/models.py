4572d004a403df4535eb06daede849c01371dd39
AsuraBot <homecat13@gmail.com>
2019-03-29 17:47:02 +0300

    work 29.03

--- landing/models.py
+++ landing/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from tinymce import HTMLField
 
 # Create your models here.
+
+
+class Slider(models.Model):
+    title = models.CharField(max_length=250, verbose_name='Заголовок слайда')
+    text = HTMLField(max_length=250, verbose_name='Текст слайда')
+
+    def get_picture_url(self, filename):
+        ext = filename.split('.')[-1]
+        filename = '%s.%s' % (self.title, ext)
+        return 'images/slides/%s' % filename
+    
+    image = models.ImageField(upload_to=get_picture_url,verbose_name='Фото слайда')
+
+    class Meta:
+        verbose_name = 'Слайд'
+        verbose_name_plural = 'Слайды'
+
+    is_active = models.BooleanField(default=True, verbose_name='Показывать слайд')
+
+    def __str__(self):
+        return '%s' % self.title
